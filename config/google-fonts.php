0a1811072de9f476d9532c00285026695bfd8da9
Gurinder Chauhan <gurinderchauhan@gmail.com>
2023-05-22 21:01:46 -0400

    WIP

--- config/google-fonts.php
+++ config/google-fonts.php
@@ -2,8 +2,5 @@
 
 // config for Apsonex/GoogleFonts
 return [
-
-
     'api_key' => env('GOOGLE_FONTS_API_KEY'),
-
 ];
--- tests/GoogleFontsTest.php
+++ tests/GoogleFontsTest.php
@@ -20,29 +20,4 @@ it('it can fetch fonts from google server via api', function () {
     $res = GoogleFonts::make()->all();
 
     expect($res)->toBeArray();
-
-    // https://fonts.googleapis.com/css2?
-    // family=Playfair+Display:wght@400;500;600;700;800;900
-    // &
-    // family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900
-    // &
-    // display=swap
-    // <link rel="preconnect" href="https://fonts.googleapis.com">
-    // <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap" rel="stylesheet">
-
-    // $data = [];
-
-    // dd(collect(File::json(basePath() . '/stubs/google-fonts.json')['items'])->groupBy('category'));
-
-    // <link rel="preconnect" href="https://fonts.googleapis.com">
-    // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    // <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
-    // dd(Arr::undot($data));
-
-    // withoutExceptionHandling();
-
-    // $res = GoogleFonts::make()->all();
-
-
 });
